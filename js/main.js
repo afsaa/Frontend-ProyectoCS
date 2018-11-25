9239ac615394c988dff77930d8ae13e95cc6392b
unknown <afsaa@utp.edu.co>
2018-11-25 17:04:16 -0500

    Usando archivos JSON para el manejo de los datos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,44 @@
 //import * as amqp from "amqplib/callback_api";
+var allSongs;
+var songsList;
+var songDetails;
 
-var songsListIndex;
 // Función para obtener el indice de la fila clickeada.
-function getRowIndex(x) {
+/*function getRowIndex(x) {
   songsListIndex = x.rowIndex - 1;
   console.log("Row index is: " + x.rowIndex);
   console.log("Index to charge the data -> " + songsListIndex);
+}*/
+
+function cargarDetalles(index) {
+  var songsListIndex = index.rowIndex - 1;
+  console.log("Indice de la canción actual: " + songsListIndex);
+
+  fetch("../details.json")
+    .then(res => res.json())
+    .then(data => {
+      songDetails = data[songsListIndex];
+      //console.log(data[0].Object.keys());
+      console.log(songDetails["Ctrl + Alt + Supr"].title);
+
+      document.getElementById("Detalle1").innerHTML =
+        songDetails["Ctrl + Alt + Supr"].title;
+
+      /*document.getElementById("Detalle2").innerHTML =
+        data.songsListKeys[songsListIndex].artist;
+      document.getElementById("Detalle3").innerHTML =
+        data.songsListKeys[songsListIndex].album;
+      document.getElementById("Detalle4").innerHTML =
+        data.songsListKeys[songsListIndex].genre;
+      document.getElementById("Detalle5").innerHTML =
+        data.songsListKeys[songsListIndex].tracknumber;
+      document.getElementById("Detalle6").innerHTML =
+        data.songsListKeys[songsListIndex].version;
+      document.getElementById("Detalle7").innerHTML =
+        data.songsListKeys[songsListIndex].date;
+      document.getElementById("Detalle8").innerHTML =
+        data.songsListKeys[songsListIndex].composer;
+      document.getElementById("Detalle9").innerHTML =
+        data.songsListKeys[songsListIndex].lyricist;*/
+    });
 }
---
+++ server.js
@@ -0,0 +1,140 @@
+var fs = require("fs");
+var amqp = require("amqplib/callback_api");
+
+// Objeto para almacenar las canciones mas adelante.
+var songsObj;
+
+var songsList;
+
+// Colas Persistentes
+var q_conexion = "conectoMesa";
+var q_votos = "atencionMesas"; //A esta cola se envía el mensaje para votar con los datos Ej. "codSitio,Mio Cid"
+
+// Colas Temporales
+var q_canciones = "canciones";
+var qSongsReceiver = makeid();
+var qVotesReceiver = makeid();
+
+function makeid() {
+  var text = "";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+function bail(err) {
+  console.error(err);
+  process.exit(1);
+}
+
+function createNewQueue(conn, newQueue) {
+  conn.createChannel(function(err, ch) {
+    ch.assertQueue(newQueue, { exclusive: true }, function(err, q) {
+      console.log(" [*] New queue %s created", q.queue);
+    });
+  });
+}
+
+function temporalQueueOperations(
+  conn,
+  code,
+  messageReceiverQueue,
+  dataReceiverQueue
+) {
+  var ok = conn.createChannel(on_open);
+  function on_open(err, ch) {
+    createNewQueue(conn, dataReceiverQueue);
+    if (err != null) bail(err);
+    // Creamos la cola temporal "q" la cual contendrá la respuesta para el sitio.
+    ch.assertQueue("", { exclusive: true }, function(err, q) {
+      console.log(
+        " [*] Waiting for messages in %s. To exit press CTRL+C",
+        q.queue
+      );
+      // Enviamos la información a la cola indicada.
+      if (dataReceiverQueue === qSongsReceiver) {
+        ch.sendToQueue(
+          messageReceiverQueue,
+          new Buffer(code + "," + dataReceiverQueue),
+          {
+            replyTo: q.queue
+          }
+        );
+        console.log(
+          "Message Receiver Queue -> " + q.queue,
+          "Songs Receiver Queue -> " + dataReceiverQueue
+        );
+      } else {
+        ch.sendToQueue(
+          messageReceiverQueue,
+          new Buffer(code + "," + "Mio Cid"),
+          {
+            replyTo: q.queue
+          }
+        );
+        console.log(
+          "Message Receiver Queue -> " + q.queue,
+          "Song Voted -> " + "Mio Cid"
+        );
+      }
+      // Consumimos los datos recibidos para administrarlos.
+      ch.consume(q.queue, function(msg) {
+        if (msg !== null) {
+          msg = msg.content.toString();
+          //var obj = JSON.parse(msg);
+          console.log(msg);
+          //console.log(obj);
+        }
+      });
+      // Consumimos la cola en la cual se reciben las canciones o los votos.
+      if (dataReceiverQueue === qSongsReceiver) {
+        ch.consume(dataReceiverQueue, function(msg) {
+          if (msg !== null) {
+            // Pasamos a string el mensaje recibido y separamos la canciones con el caracter |.
+            msg = msg.content.toString();
+            msg = msg.split("|||");
+            console.log(msg);
+            // Escribimos en el archivo json los datos.
+            fs.writeFile("details.json", "[" + msg + "]", error => {
+              /* handle error */
+            });
+            var song1 = JSON.parse(msg[0]);
+            var song2 = JSON.parse(msg[1]);
+            var song3 = JSON.parse(msg[2]);
+            var song4 = JSON.parse(msg[3]);
+
+            console.log(Object.keys(song1)[0]);
+            console.log(Object.keys(song2)[0]);
+            console.log(Object.keys(song3)[0]);
+            console.log(Object.keys(song4)[0]);
+          } else {
+            console.log("Troubles receiving the songs...");
+          }
+        });
+      } else {
+      }
+    });
+    //temporalQueueOperations(conn, q_votos, qVotesReceiver);
+  }
+}
+
+amqp.connect(
+  "amqp://www.nidal.online",
+  function(err, conn) {
+    if (err != null) bail(err);
+    // Recibimos la respuesta del servidor por medio de las colas persistentes.
+    //persistentQueueOperations(conn, q_conexion);
+    //persistentQueueOperations(conn, q_votos);
+    // Recibimos la respuesta del servidor por medio de la cola temporal.
+    temporalQueueOperations(conn, "lDz8G4", q_conexion, qSongsReceiver);
+    /*var delayInMilliseconds = 1000; //1 second
+    
+        setTimeout(function() {
+          temporalQueueOperations(conn, q_votos, qVotesReceiver);
+        }, delayInMilliseconds);*/
+  }
+);
