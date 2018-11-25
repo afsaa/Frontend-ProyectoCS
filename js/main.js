77891e5b7d355020b435604aa7e07501b3ad24d3
unknown <afsaa@utp.edu.co>
2018-11-24 19:06:25 -0500

    Actualización

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("Hola mundo");
+//import * as amqp from "amqplib/callback_api";
+
+var songsListIndex;
+// Función para obtener el indice de la fila clickeada.
+function getRowIndex(x) {
+  songsListIndex = x.rowIndex - 1;
+  console.log("Row index is: " + x.rowIndex);
+  console.log("Index to charge the data -> " + songsListIndex);
+}
